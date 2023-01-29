3d938295075c4bfa97bbbf96a7c8d528def35f0a
Serkan Şeker <serkan.seker@sekizbit.com.tr>
2023-01-29 18:20:43 +0300

    CL1 // Initial Commit

--- page1.php
+++ page1.php
@@ -2,11 +2,17 @@
 
 class X{
     function f1(){}
+    function f1_Copy(){}
     function f2(){}
+    function f2_Copy(){}
     function f3(){}
+    function f3_Copy(){}
     function f4(){}
+    function f4_Copy(){}
     function f5(){}
+    function f5_Copy(){}
     function f6(){}
+    function f6_Copy(){}
 }
 
 ?>
